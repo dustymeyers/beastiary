f0ea5cb48ec2402e437d11a4b55b314be827cb14
Dusty Meyers <dustymeyerssculpture@gmail.com>
2024-08-07 20:43:36 -0500

    using monster schema

--- beastiary/utils.py
+++ beastiary/utils.py
@@ -1,11 +1,13 @@
 from typing import List
-from .models import Monster
+from .models import Monster, MonsterSchema
 from .api_client import DnDAPIClient
 
 def load_bestiary(base_url: str) -> List[Monster]:
     client = DnDAPIClient(base_url)
     monster_data = client.get_monsters()
-    return [Monster(data) for data in monster_data]
+    schema = MonsterSchema()
+    schema_monsters = [schema.load(data) for data in monster_data]
+    return [Monster(schema_monster) for schema_monster in schema_monsters]
 
 def get_monster(base_url: str, index: str) -> Monster:
     client = DnDAPIClient(base_url)
